664337632e0cbb213cf7ae5a6992a842e2922e14
Travis <travis@travis-ci.org>
2017-05-10 13:19:56 -0300

    Incluido o teste e a logica para cadastrar sala, testes com erro por dependencia entre os testes

--- logica/sala.py
+++ logica/sala.py
@@ -1,7 +1,15 @@
 salas = []
+cod_sala = 0
 
+def _gerar_codigo():
+	global cod_sala
+	cod_sala +=1
+	return cod_sala
 
 def listar_salas():
 	return salas
 
-
+def cadastrar_sala(lotacao):
+	cod = _gerar_codigo()
+	sala = [cod, lotacao]
+	salas.append(sala)
--- testes/test_sala.py
+++ testes/test_sala.py
@@ -7,7 +7,16 @@ class TestSala(unittest.TestCase):
 		salas = sala.listar_salas()
 		self.assertEqual(0, len(salas))
 		
-	
+	def test_cadastrar_sala(self):
+		sala.cadastrar_sala(200)
+		
+		salas = sala.listar_salas()
+		
+		self.assertEqual(1, len(salas))
+		
+		s = salas[0]
+		
+		self.assertEqual(200, s[1])
 
 if __name__ == '__main__':
 	unittest.main(exit=False)
