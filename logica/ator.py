cf2e8d09fe8189f6baecec3e1e1cbf1dcedd74d3
ribeirorvs <ribeiro.rvs@hotmail.com>
2017-05-09 22:42:50 -0300

    Add teste e logica cadastrar ator
    Adicionado teste e logica para a função adicionar ator, erro ocorrendo por falta de zerar os atores antes da execução.

--- logica/ator.py
+++ logica/ator.py
@@ -1,10 +1,17 @@
 atores = []
+cod_ator = 0
+def _gerar_codigo():
+    global cod_ator
+    cod_ator += 1
+    return cod_ator
 
 def listar_atores():
     return atores
 
 def cadastrar_ator(nome, nacionalidade, idade):
-    pass
+    cod = _gerar_codigo()
+    ator = [cod, nome, nacionalidade, idade]
+    atores.append(ator)
 
 def buscar_ator(cod_ator):
     pass
--- testes/test_ator.py
+++ testes/test_ator.py
@@ -8,5 +8,11 @@ class TestAtor(unittest.TestCase):
         atores = ator.listar_atores()
         self.assertEqual(0, len(atores))
 
+    def test_cadastrar_ator(self):
+        ator.cadastrar_ator("Nome", "Nacionalidade", 20)
+
+        atores = ator.listar_atores()
+        self.assertEqual(1, len(atores))
+
 if __name__ == '__main__':
     unittest.main(exit=False)
