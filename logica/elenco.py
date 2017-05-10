311604ea0a369b0dee016b87dbbc5dae9f388179
Travis <travis@travis-ci.org>
2017-05-10 15:02:07 -0300

    Incluido o teste e a logica para adicionar ator ao elenco, testes aprensetam erro por dependencia

--- logica/elenco.py
+++ logica/elenco.py
@@ -6,3 +6,16 @@ elencos = []
 
 def listar_elenco():
 	return elencos
+	
+def adicionar_ator(cod_elenco, cod_ator, cod_filme, tipo):
+	a = ator.buscar_ator(cod_ator)
+	f = filme.buscar_filme(cod_filme)
+	elenco = [cod_elenco, cod_ator, cod_filme, tipo]
+	for i in elencos:
+		if(i[0] == cod_elenco):
+			elenco = [a, f]
+			elencos[cod_elenco] = elenco
+			return True
+	elenco = [cod_elenco, cod_ator, cod_filme, tipo]
+	elencos.append(elenco)
+	return True
--- testes/test_elenco.py
+++ testes/test_elenco.py
@@ -7,7 +7,14 @@ class TestElenco(unittest.TestCase):
 		elencos = elenco.listar_elenco()
 		self.assertEqual(0, len(elencos))
 
+	def test_adicionar_ator(self):
+		elenco.adicionar_ator(1, 1, 1, "Coadjuvante")
+		elencos = elenco.listar_elenco()
 		
+		e = elencos[0]
+		
+		self.assertEqual(1, len(elencos))
+		self.assertEqual(1, e[0])
 		
 if __name__ == '__main__':
 	unittest.main(exit=False)
