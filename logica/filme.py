7fdedbe9f92a43866770d9847b11a5b32521b72a
Rodrigo Vitor <ribeiro.rvs@hotmail.com>
2017-05-10 02:34:43 -0300

    incluido teste e logica para adicionar um filme, teste com erro por falta de remover filmes antes da execução

--- logica/filme.py
+++ logica/filme.py
@@ -1,4 +1,14 @@
 filmes = []
+cod_filme = 0
+
+def _gerar_codigo():
+    global cod_filme
+    cod_filme += 1
 
 def listar_filmes():
     return filmes
+
+def cadastrar_filme(titulo, duracao, classificacao, diretor, distribuidora, status, genero):
+    cod_filme = _gerar_codigo()
+    filme = [cod_filme, titulo, duracao, classificacao, diretor, distribuidora, status, genero]
+    filmes.append(filme)
--- testes/test_filme.py
+++ testes/test_filme.py
@@ -8,7 +8,23 @@ class TestFilme(unittest.TestCase):
 
         self.assertEqual(0, len(filmes))
 
+    def test_cadastrar_um_filme(self):
+        filme.cadastrar_filme("titulo", "duração", "classificação", "diretor", "distribuidora", "status", "genero")
 
+        atores = ator.listar_atores()
+
+        self.assertEqual(1, len(atores))
+
+        a = atores[0]
+
+        self.assertEqual("titulo", a[1])
+        self.assertEqual("duração", a[2])
+        self.assertEqual("classificação", a[3])
+        self.assertEqual("diretor", a[4])
+        self.assertEqual("distribuidora", a[5])
+        self.assertEqual("status", a[6])
+        self.assertEqual("genero", a[7])
+        
 
 if __name__ == '__main__':
     unittest.main(exit=False)
