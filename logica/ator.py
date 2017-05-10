0b1bc2ed7b34f45a810ccb61b93c52b085dcd6c6
ribeirorvs <ribeiro.rvs@hotmail.com>
2017-05-09 22:18:32 -0300

    Criação da estrutura do projeto
    Envio do arquivo main e das pastas com o inicio do modulo ator, teste ator e gui ator

---
+++ logica/ator.py
@@ -0,0 +1,17 @@
+atores = []
+
+def listar_atores():
+    pass
+
+def cadastrar_ator(nome, nacionalidade, idade):
+    pass
+
+def buscar_ator(cod_ator):
+    pass
+
+def remover_ator(cod_ator):
+    pass
+
+def remover_todos_atores():
+    pass
+
---
+++ testes/test_ator.py
@@ -0,0 +1,10 @@
+import unittest
+from logica import ator
+
+class TestAtor(unittest.TestCase):
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
