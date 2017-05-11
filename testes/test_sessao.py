e097625afa588d18e1a595b05590217c1b42ecbf
Rodrigo Vitor <ribeiro.rvs@hotmail.com>
2017-05-10 21:41:51 -0300

    Incluido o teste para cirar duas sessoes

--- testes/test_sessao.py
+++ testes/test_sessao.py
@@ -24,6 +24,21 @@ class TestSessao(unittest.TestCase):
 		self.assertEqual(None, s[1])
 		self.assertEqual(None, s[2])
 		self.assertEqual("18h", s[3])
+	
+	def test_criar_duas_sessoes(self):
+		sessao.criar_sessao(1, 1, "18h")
+		sessao.criar_sessao(2, 2, "20h")
 		
+		sessoes = sessao.listar_sessoes()
+		
+		self.assertEqual(2, len(sessoes))
+		
+		s = sessoes[1]
+		
+		self.assertEqual(2, s[0])
+		self.assertEqual(None, s[1])
+		self.assertEqual(None, s[2])
+		self.assertEqual("20h", s[3])
+	
 if __name__ == '__main__':
 	unittest.main(exit=False)
