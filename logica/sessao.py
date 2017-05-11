8608e30d7974174b90e103ddbe07b1eb5885dfd3
Rodrigo Vitor <ribeiro.rvs@hotmail.com>
2017-05-10 21:29:34 -0300

    Incluido o teste e a logica para criar sessao, testes apresentando erro de dependencia

--- logica/sessao.py
+++ logica/sessao.py
@@ -1,5 +1,17 @@
-sessoes = []
+from logica import filme
+from logica import sala
 
+sessoes = []
+cod_sessao = 0
+
+def _gerar_codigo():
+	global cod_sessao
+	cod_sessao += 1
 
 def listar_sessoes():
 	return sessoes
+	
+def criar_sessao(cod_filme, cod_sessao, horario):
+	cod = _gerar_codigo()
+	s = [cod, cod_filme, cod_sessao, horario]
+	sessoes.append(s)
--- testes/test_sessao.py
+++ testes/test_sessao.py
@@ -8,6 +8,19 @@ class TestSessao(unittest.TestCase):
 		
 		self.assertEqual(0, len(sessoes))
 	
-	
+	def test_criar_uma_sessao(self):
+		sessao.criar_sessao(1, 1, "18h")
+		
+		sessoes = sessao.listar_sessoes()
+		
+		self.assertEqual(1, len(sessoes))
+		
+		s = sessoes[0]
+		
+		self.assertEqual(1, s[0])
+		self.assertEqual(None, s[1])
+		self.assertEqual(None, s[2])
+		self.assertEqual("18h", s[3])
+		
 if __name__ == '__main__':
 	unittest.main(exit=False)
