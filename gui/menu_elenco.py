7a3a3107fe05be5272384d5add3a7f4eca2be754
Travis <travis@travis-ci.org>
2017-05-15 15:55:21 -0300

    Incluida a interface listar elencos, com erro para mostrar todos os atores

--- gui/menu_elenco.py
+++ gui/menu_elenco.py
@@ -1,5 +1,15 @@
 from logica import elenco
 
+def imprimir_elenco(elenco):
+	print("Codigo elenco: ", elenco[0][0])
+	print("Atores: ")
+	for a in elenco[0][1]:
+		print(a)
+	print("Atores: ", elenco[0][1])
+	print("Filme: ", elenco[0][2])
+	print("Tipo de ator: ", elenco[0][3])
+	print()
+
 def menu_adicionar():
 	print("\nAdicionar ator no Elenco\n")
 	cod_elenco = int(input("Codigo elenco: "))
@@ -8,7 +18,13 @@ def menu_adicionar():
 	tipo = input("Tipo de ator: ")
 	elenco.adicionar_ator(cod_elenco, cod_ator, cod_filme, tipo)
 
-
+def menu_listar():
+	print("\nAdicionar ator no Elenco\n")
+	elencos = elenco.listar_elencos()
+	for e in elencos:
+		imprimir_elenco(e)
+	
+	
 def mostrar_menu():
 	menu = ("\n----------------\n"+
             "(1) Adicionar ator no Elenco \n" +
@@ -28,7 +44,7 @@ def mostrar_menu():
 		if(option == 1):
 			menu_adicionar()
 		elif(option == 2):
-			pass
+			menu_listar()
 		elif(option == 3):
 			pass
 		elif(option == 4):
