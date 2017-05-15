42fc256b21b6fc708a248faa5f0d715f4b4fd995
Travis <travis@travis-ci.org>
2017-05-15 17:19:18 -0300

    Incluido interface buscar sessao

--- gui/menu_sessao.py
+++ gui/menu_sessao.py
@@ -3,7 +3,7 @@ from logica import sessao
 def imprimir_sessao(sessao):
 	print("Codigo Sessao: ", sessao[0])
 	print("Filme: ", sessao[1])
-	print("Sala: ", sessao[2][1])
+	print("Sala: ", sessao[2][0])
 	print("Horario: ", sessao[3])
 	print("Lotacao: ", sessao[4][1])
 	print()
@@ -17,12 +17,22 @@ def menu_criar():
 	sessao.criar_sessao(cod_sessao, cod_filme, cod_sala, horario)
 
 def menu_listar():
-	print("\nCriar Sessao\n")
+	print("\nListar Sessoes\n")
 	
 	sessoes = sessao.listar_sessoes()
 	for s in sessoes:
 		imprimir_sessao(s)
 	
+def menu_buscar():
+	print("\nBuscar Sessao\n")
+	cod_sessao = int(input("Codigo Sessao: "))
+	print()
+	s = sessao.recuperar_sessao(cod_sessao)
+	if(s == None):
+		print("Sessao nao encontrada")
+	else:
+		imprimir_sessao(s)
+
 def mostrar_menu():
 	menu = ("\n----------------\n"+
             "(1) Criar nova Sessao \n" +
@@ -43,7 +53,7 @@ def mostrar_menu():
 		elif(option == 2):
 			menu_listar()
 		elif(option == 3):
-			pass
+			menu_buscar()
 		elif(option == 4):
 			pass
 		elif(option == 5):
