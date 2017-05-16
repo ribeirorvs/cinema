50fce71dde5518b212e0ad6cd583e0fc53f6bb5d
Rodrigo Vitor <ribeiro.rvs@hotmail.com>
2017-05-16 01:02:48 -0300

    Adicionado interface buscar ingresso

--- gui/menu_ingresso.py
+++ gui/menu_ingresso.py
@@ -1,10 +1,31 @@
 from logica import ingresso
 
+def imprimir_ingresso(ingresso):
+	print("Codigo Ingresso: ", ingresso[0])
+	print("Sessao: ", ingresso[1])
+
 def menu_vender_meia():
-	print("\nCadastrar Ator\n")
+	print("\nVender ingresso Meia\n")
 	cod_ingresso = int(input("Codigo Ingresso: "))
 	cod_sessao = int(input("Codigo Sessao: "))
 	ingresso.venda_ingresso_meia(cod_ingresso, cod_sessao)
+
+def menu_vender_inteira():
+	print("\nVender ingresso Inteira\n")
+	cod_ingresso = int(input("Codigo Ingresso: "))
+	cod_sessao = int(input("Codigo Sessao: "))
+	ingresso.venda_ingresso_inteira(cod_ingresso, cod_sessao)
+
+def menu_buscar():
+	print("\nBuscar Ingresso\n")
+	cod_sessao = int(input("Codigo Sessao: "))
+	print()
+	i = ingresso.buscar_ingresso(cod_sessao	)
+	if(i == None):
+		print("Ingresso nao encontrado")
+	else:
+		imprimir_ingresso(i)
+	
 
 def mostrar_menu():
 	menu = ("\n----------------\n"+
@@ -24,9 +45,9 @@ def mostrar_menu():
 		if(option == 1):
 			menu_vender_meia()
 		elif(option == 2):
-			pass
+			menu_vender_inteira()
 		elif(option == 3):
-			pass
+			menu_buscar()
 		elif(option == 4):
 			pass
 		elif(option == 5):
